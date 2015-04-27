3363f19dfd509cfc4fe9b36c34cba149cb8765f9
cdascoli <cdascoli@gmail.com>
2015-04-26 17:32:57 -0700

    Committed by Udacity

---
+++ html_generator.py
@@ -0,0 +1,65 @@
+def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def get_title(concept):
+    start_location = concept.find('TITLE:')
+    end_location = concept.find('DESCRIPTION:')
+    title = concept[start_location+7 : end_location-1]
+    return title
+
+def get_description(concept):
+    start_location = concept.find('DESCRIPTION:')
+    description = concept[start_location+13 :]
+    return description
+
+def get_concept_by_number(text, concept_number):
+    counter = 0
+    while counter < concept_number:
+        counter = counter + 1
+        next_concept_start = text.find('TITLE:')
+        next_concept_end   = text.find('TITLE:', next_concept_start + 1)
+        concept = text[next_concept_start:next_concept_end]
+        text = text[next_concept_end:]
+    return concept
+        
+
+TEST_TEXT = """TITLE: Telling Computers What to Do
+DESCRIPTION: Programming is the core of computer science. 
+It is a precise set of steps, in a precise sequence that 
+tells the computer what to do.
+TITLE: Variable & Strings
+DESCRIPTION: A variable is an assignment statement. A String
+is a sequence of characters surrounded by quotes.
+TITLE: Functions
+DESCRIPTION: A Function is a code that takes an input, does
+something with it, and produces an output, or result."""
+
+
+def generate_all_html(text):
+    current_concept_number = 1
+    concept = get_concept_by_number(text, current_concept_number)
+    all_html = ''
+    while concept != '':
+        title = get_title(concept)
+        description = get_description(concept)
+        concept_html = generate_concept_HTML(title, description)
+        all_html = all_html + concept_html
+        current_concept_number = current_concept_number + 1
+        concept = get_concept_by_number(text, current_concept_number)
+    return all_html
+
+
+print generate_all_html(TEST_TEXT)
